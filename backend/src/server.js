3d1706883cd869b207e1c73d573ef93c72a696c7
pro-ject <abhinavrana028@gmail.com>
2025-11-23 00:41:17 +0530

    initialize project

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import authRoutes from "./routes/auth.route.js";
+
+
+const app = express();
+const PORT = process.env.PORT 
+
+
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
