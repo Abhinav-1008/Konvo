088df41c0e5ae8225b497d154b2fd5db56617dc4
pro-ject <abhinavrana028@gmail.com>
2025-11-23 10:55:21 +0530

    change the req method in auth.controller file

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 
 
 const app = express();
-const PORT = process.env.PORT 
+const PORT = process.env.PORT ||5000;
 
 
 
@@ -14,4 +14,4 @@ app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}); 
